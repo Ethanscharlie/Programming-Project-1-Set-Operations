8f979c3ed7e4adb0483ae6caaffd7489d93dd816
Ethan J. Hadley <ethanscharlie@protonmail.com>
2026-02-15 17:25:07 -0700

    Draft of stubs

---
+++ operations.py
@@ -0,0 +1,49 @@
+from typing import TypeAlias
+
+Set: TypeAlias = list[bool]
+Multiset: TypeAlias = dict[str, int]
+
+###################################################
+# PART 1
+###################################################
+
+
+def complement(a: Set) -> Set:
+    pass
+
+
+def union(a: Set, b: Set) -> Set:
+    pass
+
+
+def intersection(a: Set, b: Set) -> Set:
+    pass
+
+
+def difference(a: Set, b: Set) -> Set:
+    pass
+
+
+def symmetricDifference(a: Set, b: Set) -> Set:
+    pass
+
+
+###################################################
+# PART 2
+###################################################
+
+
+def multisetUnion(a: Multiset, b: Multiset) -> Multiset:
+    pass
+
+
+def multisetIntersection(a: Multiset, b: Multiset) -> Multiset:
+    pass
+
+
+def multisetDifference(a: Multiset, b: Multiset) -> Multiset:
+    pass
+
+
+def multisetSum(a: Multiset, b: Multiset) -> Multiset:
+    pass
