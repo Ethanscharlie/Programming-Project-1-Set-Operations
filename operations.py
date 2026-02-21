9ac43c176d1ed03f99457a42be6f4f072a72dda1
CosmicWatermeln <165712076+CosmicWatermeln@users.noreply.github.com>
2026-02-21 00:39:58 -0700

    part 1 implemented but not tested
    I have implemented, but not tested all of the functions in part 1. PLEASE let me know if my syntax is wacky. I will clean up code duplication later.

--- operations.py
+++ operations.py
@@ -9,15 +9,29 @@ UNIVERSAL_SET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
 # PART 1
 ###################################################
 
-
+#complement: everything outside of a in the universal set
+#ex: a = [a,b]
+# complement(a) = [c, d, e, f, g, h, i, j, k] 
 def complement(a: SubSet) -> list[bool]:
-    pass
+    out = list[bool]
+    out = [0,0,0,0,0,0,0,0,0,0,0,0]
 
+    for i in range(12):
+        out[i] = !a[i]
+    #if a[i] == 0, then out[i] = 1 and vice versa
+    return out[i]
 
 def union(a: SubSet, b: SubSet) -> list[bool]:
-    pass
+    out = list[bool]
+    out = [0,0,0,0,0,0,0,0,0,0,0,0]
+
+    for i in range(12):
+        out[i] = (a[i] or b[i])
+    return out[i]
 
 
+# intersection: 
+# all elements shared between sets a and b
 def intersection(a: SubSet, b: SubSet) -> list[bool]:
     pass
 
